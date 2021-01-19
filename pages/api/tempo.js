8de348d625121da466600dcbeae7689dd4e843d3
Gustavo Barbosa Correia <gustavo.correia@writesys.com.br>
2021-01-19 17:15:52 -0300

    api-

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -6,7 +6,10 @@ async function tempo(request, response) {
 
     const subscribersResponse =  await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersResponseJson = await subscribersResponse.json();
-    const inscritos = subscribersResponseJson.total_subscribers;    
+    const inscritos = subscribersResponseJson;    
+
+    //COMANDO PARA CAHCEAR NA VERCEL    
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString(),
