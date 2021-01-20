7cdc3d2cd223dc047d158fb66fbc792f1f962438
Gustavo Barbosa Correia <gustavo.correia@writesys.com.br>
2021-01-20 11:22:49 -0300

    teste

--- pages/sobre.js
+++ pages/sobre.js
@@ -5,7 +5,7 @@ function Sobre() {
         <h1>Home Top</h1>
 
         <Link href="/">
-            <a>Acessar Página Home</a>
+            <a>Acessar Página Home teste</a>
         </Link>
         
         </div>        
